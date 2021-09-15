764b829219d8c8147e0325dbdd5212fa1fd81936
JHoo0118 <kik3078@metabuild.co.kr>
2021-09-15 10:32:16 +0900

    사용자 관리 개발

--- src/main/java/com/mesim/sc/api/rest/admin/user/UserRestController.java
+++ src/main/java/com/mesim/sc/api/rest/admin/user/UserRestController.java
@@ -4,12 +4,16 @@ import com.mesim.sc.api.ApiResponseDto;
 import com.mesim.sc.api.rest.admin.AdminRestController;
 import com.mesim.sc.exception.BackendException;
 import com.mesim.sc.service.CrudService;
+import com.mesim.sc.service.admin.user.UserDto;
 import com.mesim.sc.service.admin.user.UserService;
+import com.mesim.sc.service.admin.vocal.VocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -30,6 +34,78 @@ public class UserRestController extends AdminRestController {
             return new ApiResponseDto(true, ((UserService) service).changePassword(o));
         } catch (Exception e) {
             throw new BackendException(this.name + " 비밀번호 변경 중 오류발생", e);
+        }
+    }
+
+    // 사용자 전체 조회
+    @RequestMapping(value = "listPage/{regId}/{regGroupId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponseDto getList(@PathVariable(value = "regId") String regId,
+                                  @PathVariable(value = "regGroupId") String regGroupId,
+                                  @RequestParam(value = "select", required = false) String[] select,
+                                  @RequestParam(value = "index") int index,
+                                  @RequestParam(value = "size") int size,
+                                  @RequestParam(value = "sortProperties", required = false) String[] sortProperties,
+                                  @RequestParam(value = "keywords", required = false) String[] keywords,
+                                  @RequestParam(value = "searchOp", required = false) String searchOp,
+                                  @RequestParam(value = "fromDate", required = false) String fromDate,
+                                  @RequestParam(value = "toDate", required = false) String toDate
+    ) throws BackendException {
+        try {
+            return new ApiResponseDto(true, ((UserService) this.service).getListPage(regId, regGroupId, select, index, size, sortProperties, keywords, searchOp, fromDate, toDate));
+        } catch (Exception e) {
+            throw new BackendException(this.name + " 페이지 목록 조회 중 오류발생", e);
+        }
+    }
+
+    /**
+     * 사용자 추가
+     *
+     * @param dto 등록할 데이터 DTO
+     * @return 성공/실패 여부
+     */
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.ALL_VALUE)
+    public ApiResponseDto add(
+            @RequestPart(value = "data") Object dto,
+            Authentication authentication
+    ) throws BackendException {
+        try {
+            String userId = authentication.getPrincipal().toString();
+            return new ApiResponseDto(true, ((UserService) service).add(dto));
+        } catch (Exception e) {
+            throw new BackendException(this.name + " 등록 중 오류발생", e);
+        }
+    }
+
+    /**
+     * 사용자 수정
+     *
+     * @param dto 수정할 데이터 DTO
+     * @return 성공/실패 여부
+     */
+    @RequestMapping(value = "", method = RequestMethod.PUT, produces = MediaType.ALL_VALUE)
+    public ApiResponseDto update(
+            @RequestPart(value = "data") Object dto,
+            Authentication authentication
+    ) throws BackendException {
+        try {
+            String userId = authentication.getPrincipal().toString();
+            return new ApiResponseDto(true, ((UserService) service).update(dto));
+        } catch (Exception e) {
+            throw new BackendException(this.name + " 등록 중 오류발생", e);
+        }
+    }
+
+    /**
+     * 사용자 승인
+     *
+     * @return 성공/실패 여부
+     */
+    @RequestMapping(value="grant/{id}", method = RequestMethod.PUT, produces = MediaType.ALL_VALUE)
+    public ApiResponseDto update(@PathVariable("id") String userId) throws BackendException {
+        try {
+            return new ApiResponseDto(true, ((UserService) service).grantUser(userId));
+        } catch (Exception e) {
+            throw new BackendException(this.name + " 승인 중 오류발생", e);
         }
     }
 
--- src/main/java/com/mesim/sc/service/admin/user/UserService.java
+++ src/main/java/com/mesim/sc/service/admin/user/UserService.java
@@ -1,10 +1,11 @@
 package com.mesim.sc.service.admin.user;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import com.mesim.sc.constants.CommonConstants;
 import com.mesim.sc.exception.BackendException;
+import com.mesim.sc.exception.ExceptionHandler;
+import com.mesim.sc.repository.PageWrapper;
 import com.mesim.sc.repository.rdb.CrudRepository;
+import com.mesim.sc.repository.rdb.admin.AdminSpecs;
 import com.mesim.sc.repository.rdb.admin.user.User;
 import com.mesim.sc.repository.rdb.admin.user.UserRepository;
 
@@ -15,17 +16,17 @@ import com.mesim.sc.util.PwEncoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -43,7 +44,7 @@ public class UserService extends AdminService {
 
     @PostConstruct
     public void init () {
-        this.searchFields = new String[]{"id", "name", "authorityName", "groupName"};
+        this.searchFields = new String[]{"id", "name", "authorityName", "groupName", "status"};
         this.joinedSortField = new String[]{"authority", "group"};
         this.excludeColumn = new String[]{"password", "imgBase64Str"};
         this.root.put("id", CommonConstants.USER_ROOT_ID);
@@ -52,6 +53,67 @@ public class UserService extends AdminService {
         this.addRefEntity("group", "name");
 
         super.init();
+    }
+
+    // 전체 목록 조회
+    public PageWrapper getListPage(String regId, String regGroupId, String[] select, int index, int size, String[] sortProperties, String[] keywords, String searchOp, String fromDate, String toDate) throws BackendException {
+        Specification<Object> spec = null;
+
+        if (!regGroupId.equals(Integer.toString(CommonConstants.GROUP_ROOT_ID))) {
+            if (regGroupId.equals(Integer.toString(CommonConstants.GROUP_CHILL_ROOT_ID))) {
+                spec = AdminSpecs.regGroupId(CommonConstants.GROUP_CHILL_ID).or(AdminSpecs.regGroupId(CommonConstants.GROUP_CHILL_ROOT_ID));
+            } else if (regGroupId.equals(Integer.toString(CommonConstants.GROUP_FKMP_ROOT_ID))) {
+                spec = AdminSpecs.regGroupId(CommonConstants.GROUP_FKMP_ID).or(AdminSpecs.regGroupId(CommonConstants.GROUP_FKMP_ROOT_ID));
+            } else {
+                spec = AdminSpecs.regId(regId);
+            }
+        }
+
+        PageRequest pageRequest = this.getPageRequest(index, size, sortProperties);
+        Specification<Object> pageSpec = this.getSpec(select, keywords, searchOp, fromDate, toDate);
+
+        if (pageSpec != null) {
+            spec = spec == null ? pageSpec : spec.and(pageSpec);
+        }
+
+        Page<Object> page = this.repository.findAll(spec, pageRequest);
+        PageWrapper result = new PageWrapper(page);
+        final AtomicInteger seq = new AtomicInteger(1);
+
+        List<Object> list = page
+                .get()
+                .map(ExceptionHandler.wrap(entity -> this.toDto(entity, seq.getAndIncrement() + (result.getNumber() * size))))
+                .collect(Collectors.toList());
+
+        result.setList(list);
+
+        return result;
+    }
+
+    // 사용자 추가
+    public Object add(Object data) throws BackendException {
+        UserDto savedUserDto = (UserDto) this.save(data);
+        return savedUserDto;
+    }
+
+    // 사용자 수정
+    public Object update(Object data) throws BackendException {
+        UserDto updatedUserDto = (UserDto) this.modify(data);
+        return updatedUserDto;
+    }
+
+    // 사용자 삭제
+    @Override
+    public boolean delete(Object o) {
+        Map<String, Object> map = (Map<String, Object>) o;
+        List<Object> deleteObjects = ((List<Object>) map.get("data"))
+                .stream()
+                .map(ExceptionHandler.wrap(object -> this.toEntity(object)))
+                .collect(Collectors.toList());
+
+        this.repository.deleteAll(deleteObjects);
+
+        return true;
     }
 
     public Object changePassword(Object o) throws BackendException {
@@ -101,6 +163,19 @@ public class UserService extends AdminService {
 //            throw new BackendException("존재하지 않는 사용자입니다.");
 //        }
 //    }
+
+    public Object grantUser(String userId) throws BackendException {
+        Optional<User> optUser = ((UserRepository) this.repository).findById(userId);
+
+        if (optUser.isPresent()) {
+            UserDto userDto = this.mapper.convertValue(optUser.get(), UserDto.class);
+            userDto.setStatus("Y");
+            userDto.setPasswordEncoded(true);
+            return this.save(userDto);
+        } else {
+            throw new BackendException("존재하지 않는 사용자입니다.");
+        }
+    }
 
     public List<UserDto> connectUser() {
         return ((UserRepository) this.repository).findAllById(JwtTokenProvider.getTokenIds())
